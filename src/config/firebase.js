7257b457493bf053a52dcd5413fc1362f653dd8b
rahulismyid@gmail.com <rahulismyid@gmail.com>
2024-04-18 13:03:10 -0400

    file change

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { initializeFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { initializeFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const app = initializeApp({
-  apiKey: "BIzaSyBjX2eRB7w9qNLZYBkkXIcg4IDQZo_kwNw",
-  authDomain: "my-patient-record-system-21212.firebaseapp.com",
-  projectId: "my-patient-record-system-21212",
-  storageBucket: "my-patient-record-system-21212.appspot.com",
-  messagingSenderId: "738707582975",
-  appId: "2:638707582975:web:3dbfed273202a7411649ca"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 // Initialize Firebase
@@ -19,6 +19,6 @@ const app = initializeApp({
 
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
-	experimentalForceLongPolling: true,
+  experimentalForceLongPolling: true,
 });
 export default app;
